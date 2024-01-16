95928d3c4e5fdc46eeb8777ef15fe0fa6a27135b
kranthi kumar <g.kranthi2507@gmail.com>
2024-01-16 19:06:16 +0530

    upvote article

--- src/server.js
+++ src/server.js
@@ -1,10 +1,32 @@
 import express from 'express';
 
-const app = express();
+let articlesInfo = [{
+    name: 'learn-react',
+    upvotes: 0,
+},
+{
+    name: 'learn-node',
+    upvotes: 0,
+},
+{
+    name: 'mongodb',
+    upvotes: 0,
+}]
 
-app.get('/hello', (req, res) => {
-    res.send('Hello!')
-});
+const app = express();
+app.use(express.json());
+
+app.put('/api/articles/:name/upvote', (req, res) => {
+    const { name } = req.params;
+    const article = articlesInfo.find(a => a.name === name);
+    if (article) {
+        article.upvotes += 1;
+        res.send(`The ${name} article now has ${article.upvotes}`)
+    } else {
+        res.send('The article doen\'t exist');
+    }
+    
+})
 
 app.listen(8000, () => {
     console.log("Server is listening on port 8000");
