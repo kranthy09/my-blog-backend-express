6b281040d068ee6e68508e9a6f860f6c44a3f0a7
kranthi kumar <g.kranthi2507@gmail.com>
2024-02-12 21:00:13 +0530

    mongo db connection

--- src/server.js
+++ src/server.js
@@ -1,33 +1,58 @@
 import express from 'express';
-
-let articlesInfo = [{
-    name: 'learn-react',
-    upvotes: 0,
-},
-{
-    name: 'learn-node',
-    upvotes: 0,
-},
-{
-    name: 'mongodb',
-    upvotes: 0,
-}]
+import { db, connectToDb } from './db.js';
 
 const app = express();
 app.use(express.json());
 
-app.put('/api/articles/:name/upvote', (req, res) => {
+app.get('/api/articles/:name', async (req, res) => {
     const { name } = req.params;
-    const article = articlesInfo.find(a => a.name === name);
+
+    const article = await db.collection('articles').findOne({ name });
     if (article) {
-        article.upvotes += 1;
-        res.send(`The ${name} article now has - ${article.upvotes}`)
+        res.json(article);
+    } else {
+        res.sendStatus(404);
+    }
+
+})
+
+app.put('/api/articles/:name/upvote', async (req, res) => {
+    const { name } = req.params;
+
+    await db.collection('articles').updateOne({ name }, {
+        $inc: { upvotes: 1 },
+    });
+    const article = await db.collection('articles').findOne({ name })
+    if (article) {
+        res.json(article);
     } else {
         res.send('The article doen\'t exist');
     }
-    
+
 })
 
-app.listen(8000, () => {
-    console.log("Server is listening on port 8000");
-});
+app.post('/api/articles/:name/comments', async (req, res) => {
+    const { name } = req.params;
+    const { postedBy, text } = req.body;
+
+    await db.collection('articles').updateOne({ name }, {
+        $push: { comments: { postedBy, text } }
+    });
+    const article = await db.collection('articles').findOne({ name })
+    if (article) {
+        res.json(article);
+    } else {
+        res.send('That article does\'nt exist');
+    }
+
+
+
+})
+
+connectToDb(() => {
+    console.log('connected to database...')
+    app.listen(8000, () => {
+        console.log("Server is listening on port 8000");
+    });
+})
+
