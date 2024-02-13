c3bb4fcc1c72122a720f4a28c3cf4ecfdc1a66ee
kranthi kumar <g.kranthi2507@gmail.com>
2024-02-14 02:33:18 +0530

    end point authentication

--- src/server.js
+++ src/server.js
@@ -1,30 +1,42 @@
 import fs from 'fs';
+import path from 'path';
 import admin from 'firebase-admin';
 import express from 'express';
+import 'dotenv/config';
 import { db, connectToDb } from './db.js';
 
-const credentails = JSON.parse(
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const credentials = JSON.parse(
     fs.readFileSync('./credentials.json')
 );
-
-admin.initializeApp(
-    { credentail: admin.credential.cert(credentails), }
-)
+admin.initializeApp({
+    credential: admin.credential.cert(credentials),
+});
 
 const app = express();
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '../build')));
+
+app.get(/^(?!\/api).+/, (req, res) => {
+    res.sendFile(path.join(__dirname, '../build/index.html'));
+})
 
 app.use(async (req, res, next) => {
     const { authtoken } = req.headers;
 
     if (authtoken) {
         try {
-            req.user = await admin.auth.verifyIdToken(authtoken)
+            req.user = await admin.auth().verifyIdToken(authtoken);
         } catch (e) {
             return res.sendStatus(400);
         }
     }
+
     req.user = req.user || {};
+
     next();
 });
 
@@ -33,18 +45,19 @@ app.get('/api/articles/:name', async (req, res) => {
     const { uid } = req.user;
 
     const article = await db.collection('articles').findOne({ name });
+
     if (article) {
         const upvoteIds = article.upvoteIds || [];
-        article.canUpvote = uid && !upvoteIds.includes(uid)
+        article.canUpvote = uid && !upvoteIds.includes(uid);
         res.json(article);
     } else {
         res.sendStatus(404);
     }
-
-})
+});
 
 app.use((req, res, next) => {
-    if (req.use) {
+    console.log('use, req.user', req.user);
+    if (req.user) {
         next();
     } else {
         res.sendStatus(401);
@@ -53,47 +66,52 @@ app.use((req, res, next) => {
 
 app.put('/api/articles/:name/upvote', async (req, res) => {
     const { name } = req.params;
+    console.log('req.user', req.user);
     const { uid } = req.user;
-    const article = await db.collection('articles').findOne({ name })
+    console.log('uid', uid);
+
+    const article = await db.collection('articles').findOne({ name });
+
     if (article) {
         const upvoteIds = article.upvoteIds || [];
-        const canUpvote = uid && !upvoteIds.include(uid)
+        const canUpvote = uid && !upvoteIds.includes(uid);
+
         if (canUpvote) {
             await db.collection('articles').updateOne({ name }, {
                 $inc: { upvotes: 1 },
-                $push: { votesIds: uid }
+                $push: { upvoteIds: uid },
             });
         }
-        const updatedArticle = await db.collection('articles').findOne({ name })
+
+        const updatedArticle = await db.collection('articles').findOne({ name });
         res.json(updatedArticle);
     } else {
-        res.send('The article doen\'t exist');
+        res.send('That article doesn\'t exist');
     }
-
-})
+});
 
 app.post('/api/articles/:name/comments', async (req, res) => {
     const { name } = req.params;
     const { text } = req.body;
     const { email } = req.user;
+
     await db.collection('articles').updateOne({ name }, {
-        $push: { comments: { postedBy: email, text } }
+        $push: { comments: { postedBy: email, text } },
     });
-    const article = await db.collection('articles').findOne({ name })
+    const article = await db.collection('articles').findOne({ name });
+
     if (article) {
         res.json(article);
     } else {
-        res.send('That article does\'nt exist');
+        res.send('That article doesn\'t exist!');
     }
+});
 
-
-
-})
+const PORT = process.env.PORT || 8000;
 
 connectToDb(() => {
-    console.log('connected to database...')
-    app.listen(8000, () => {
-        console.log("Server is listening on port 8000");
+    console.log('Successfully connected to database!');
+    app.listen(PORT, () => {
+        console.log('Server is listening on port ' + PORT);
     });
 })
-
